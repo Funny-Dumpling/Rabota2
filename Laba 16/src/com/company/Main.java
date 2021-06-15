ee8d6b8b9e962a82f7e8f8f4f65cd5971486dbad
Funny-Dumpling <77583994+Funny-Dumpling@users.noreply.github.com>
2021-06-15 08:29:16 +0300

    Add files via upload

---
+++ Laba 16/src/com/company/Main.java
@@ -0,0 +1,81 @@
+package com.company;
+interface BasicCharging{
+        public void There();
+        public void ChargOn();
+        public void ChargOff();
+}
+interface Charging380V{
+        public void There();
+        public void ChargOn();
+        public void ChargOff();
+}
+class Charging220V implements BasicCharging{
+        final public int charge = 81;
+        @Override
+        public void There(){
+                System.out.println("Зарядка есть");
+        }
+        @Override
+        public void ChargOn(){
+                System.out.println("Зарядка работает+");
+        }
+        @Override
+        public void ChargOff(){
+                System.out.println("Зарядка не работает");
+        }
+}
+class Charg380V implements Charging380V{
+        final public int charge = 50;
+        @Override
+        public void There(){
+                System.out.println("Зарядка на 380В есть");
+        }
+        @Override
+        public void ChargOn(){
+                System.out.println("Зарядка на 380В работает");
+        }
+        @Override
+        public void ChargOff(){
+                System.out.println("Зарядка на 380В не работает");
+        }
+}
+class ChargAdapter implements BasicCharging{
+        Charging380V charging380V;
+        public ChargAdapter(Charging380V charging380V){
+                this.charging380V = charging380V;
+        }
+        @Override
+        public void There(){
+                charging380V.There();
+        }
+        @Override
+        public void ChargOn(){
+                charging380V.ChargOn();
+        }
+        @Override
+        public void ChargOff(){
+                charging380V.ChargOff();
+        }
+}
+class Charging{
+        private BasicCharging device;
+        public Charging(BasicCharging device){
+                this.device = device;
+        }
+        public void work(){
+                device.There();
+                device.ChargOn();
+                device.ChargOff();
+        }
+}
+public class Main{
+        public static void main(String[] args){
+                BasicCharging basic_charging = new Charging220V();
+                Charging ch220 = new Charging(basic_charging);
+                ch220.work();
+                ChargAdapter charger380V = new ChargAdapter(new Charg380V());
+                Charging ch380 = new Charging(charger380V);
+                ch380.work();
+        }
+}
+
